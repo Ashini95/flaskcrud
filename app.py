dfb445d9abca0b563bad5f903efadab4c7b58e57
Ashini95 <ashiddk95@gmail.com>
2021-02-19 10:26:54 +0530

    Add existing file

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
 app.secret_key = "flash message"
 
@@ -14,7 +13,12 @@ mysql = MySQL(app)
 
 @app.route('/')
 def Index() :
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM students")
+    data = cur.fetchall()
+    cur.close()
+
+    return render_template('index.html', students = data)
 
 @app.route('/insert', methods = ['GET','POST'])
 def insert():
@@ -28,8 +32,36 @@ def insert():
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO STUDENTS (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
         mysql.connection.commit()
+
         return redirect(url_for('Index'))
 
+@app.route('/update', methods = ['GET','POST'])
+def update():
+    if request.method == 'POST':
+        id_data = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        cur = mysql.connection.cursor()
+        cur.execute("""
+        UPDATE students
+        SET name=%s, email=%s, phone=%s
+        WHERE id=%s
+        """,(name, email, phone, id_data))
+        flash("Data Updated Successfully")
+        mysql.connection.commit()
+
+        return redirect(url_for('Index'))
+
+@app.route('/delete/<string:id_data>', methods = ['GET','POST'])
+def delete(id_data):
+    flash("Data Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM students WHERE id=%s",(id_data))
+    mysql.connection.commit()
+
+    return redirect(url_for('Index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
